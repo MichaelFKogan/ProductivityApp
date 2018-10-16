4740f2451fc1262da535a63d098a35fbe5c28cfb
Michael Kogan <michaelkogan@MichaelsMacBook.fios-router.home>
2018-10-16 08:12:45 -0400

    add more icons and ui

--- assets/js/timers.js
+++ assets/js/timers.js
@@ -1,27 +1,49 @@
 var timer;
 var stop = false;
+var play = false;
 
 // CURRENT TIMER
 
 // ON START
     $('#start1').click(function(e) {
-      if($('#startTime1').html() == '' && stop == false){
-    
-    //ADD TIME STAMP
-    var stamp1 = moment().format("h:mm:ss A")    
 
-    function displayTime() {
-        var time = moment().format('h:mm:ss A');
-        timer = setTimeout(displayTime, 1000);
-    
-    $('#startTime1').html(stamp1);  
-    $('#stopTime1').html(time);  
-    }
+// COUNT UP TIMER
 
-    $(document).ready(function() {
-        displayTime();
-    });
-    }
+function startTimer()
+{
+    var startTimestamp = moment().startOf("day");
+    
+    setInterval(function() {
+        
+        startTimestamp.add(1, 'second');
+        
+        document.getElementById('timer1').innerHTML = startTimestamp.format('HH:mm:ss');
+    }, 1000);
+}
+
+startTimer();
+
+        
+        if(stop==false && play==false){
+        //ADD TIME STAMP
+            var stamp1 = moment().format("h:mm:ss A");
+        // TIME STAMP
+            $('#startTime1').html(stamp1); 
+        } 
+
+        if(stop==false && play==false || play==true){
+        // CLOCK
+            function displayTime() {
+                var time = moment().format('h:mm:ss A');
+                timer = setTimeout(displayTime, 1000);
+                $('#stopTime1').html(time);
+            }
+            $(document).ready(function() {
+                displayTime();
+            });
+        }
+        stop = false;
+        play = true;
     });
 
 
@@ -30,10 +52,11 @@ var stop = false;
 //ADD FINISHED TIMESTAMP  
       $('#stop1').click(function() {
         if(stop==false){
-      stop = true;
       clearTimeout(timer);
       var stamp3 = moment().format("h:mm:ss A")
       $('#stopTime1').html(stamp3);
+      stop = true;
+      play = true;
       }
       });
 
@@ -42,9 +65,11 @@ var stop = false;
 
 // RESET FIELDS
       $('#reset1').click(function() {
+      clearTimeout(timer);
       $('#startTime1').html('')
       $('#stopTime1').html('')
       stop = false;
+      play = false;
       });
 
 
